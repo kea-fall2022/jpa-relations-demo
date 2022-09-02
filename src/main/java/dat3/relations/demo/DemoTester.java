3c14a6301e65e6fdbff5d4b15434ccd7e41844a4
plaul <lmor@kea.dk>
2022-09-02 11:11:54 +0200

    first

--- src/main/java/dat3/relations/demo/DemoTester.java
+++ src/main/java/dat3/relations/demo/DemoTester.java
@@ -1,5 +1,6 @@
 package dat3.relations.demo;
 
+import dat3.relations.demo.entity.Person;
 import dat3.relations.demo.repositories.AddressRepository;
 import dat3.relations.demo.repositories.PersonRepository;
 import org.springframework.boot.CommandLineRunner;
@@ -21,6 +22,12 @@ public class DemoTester implements CommandLineRunner {
     System.out.println("I'm called when enver the program is started");
 
     //This is where we will add all the demo code
+
+    //This is where we will add all the demo code
+    Person p1 = new Person("Peter","pe@a.dk","hemlig");
+    Person p2 = new Person("Janne","ja@a.dk","super-hemlig");
+    personRepository.save(p1);
+    personRepository.save(p2);
 
     System.out.println("Hit Enter to stop program");
     System.in.read();
